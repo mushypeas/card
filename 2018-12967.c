14b678663172323053e386aaf7a05cfe70f5b445
mushypeas <qkrwoans2@gmail.com>
2018-11-30 00:27:25 +0900

    Duh

--- 2018-12967.c
+++ 2018-12967.c
@@ -9,17 +9,22 @@
 int key_in;
 
 char cards[4][4];		// Cards
-int on[2] = {0,0};
+int on[2] = {0,0};      // The card the player is on
+int menu = 0;           // 0: game start 1: exit
+int screen = 0;         // 0: menu 1: game
 
 void initialize();
-void display();
+void display(int s);
+void display_menu();
 void display_gameboard();
 int box_char(int x);
 
 int main() {	
 
 	initialize();
-
+    start_color();
+    init_color(8, 999, 999, 999);
+    init_pair(1, COLOR_BLACK, 8);
 	while(1) {
 		int c = 0;
 		for(int i=0; i<4; i++)
@@ -28,46 +33,58 @@ int main() {
         fflush(stdout);
 		display();
 		key_in = getch();
-		if(key_in == up){
-			if(on[0] == 0)
-				on[0] = 3;
-			else
-				on[0] -= 1;
-		}
-		else if(key_in == down){
-			if(on[0] == 3)
-				on[0] = 0;
-			else
-				on[0] += 1;
-		}
-		else if(key_in == left){
-			if(on[1] == 0)
-				on[1] = 3;
-			else
-				on[1] -= 1;
-		}
-		else if(key_in == right){
-			if(on[1] == 3)
-				on[1] = 0;
-			else
-				on[1] += 1;
-		}
-		else if(key_in == ' '){
-			if(on[0] == 3){
+		if(screen){     //If in game
+			if(key_in == up){
+				if(on[0] == 0)
+					on[0] = 3;
+				else
+					on[0] -= 1;
+			}
+			else if(key_in == down){
+				if(on[0] == 3)
+					on[0] = 0;
+				else
+					on[0] += 1;
+			}
+			else if(key_in == left){
+				if(on[1] == 0)
+					on[1] = 3;
+				else
+					on[1] -= 1;
+			}
+			else if(key_in == right){
 				if(on[1] == 3)
-					on[0] = 0, on[1] = 0;
+					on[1] = 0;
 				else
 					on[1] += 1;
 			}
-			else{
-				if(on[1] == 3)
-					on[0] += 1, on[1] = 0;
-				else
-					on[1] += 1;
+			else if(key_in == ' '){
+				if(on[0] == 3){
+					if(on[1] == 3)
+						on[0] = 0, on[1] = 0;
+					else
+						on[1] += 1;
+				}
+				else{
+					if(on[1] == 3)
+						on[0] += 1, on[1] = 0;
+					else
+						on[1] += 1;
+				}
 			}
+			else if(key_in == 'Q' || key_in == 'q')
+				break;
 		}
-		else if(key_in == 'Q' || key_in == 'q')
-			break;
+		else{          //If in menu
+			if(key_in == up)
+				menu=0;
+			else if(key_in == down)
+				menu=1;
+			else if(key_in == ' ')
+				menu?menu=0:menu=1;
+			else if(key_in == 'Q' || key_in == 'q')
+				break;
+		}
 	}
 
 	endwin();	// Do NOT modify
@@ -84,12 +101,54 @@ void initialize() {
 	display();
 }
 
-void display() {
+void display(int s) {
 	clear();
-	display_gameboard();
+	if(s)
+		display_menu();
+	else
+		display_gameboard();
 	refresh();
 }
-
+void display_menu(){
+    mvprintw(0,0,"*Monospaced font recommended");
+    int y = LINES/2 - 2, x = COLS/2 - 2;
+    move(y-2, x);
+    printw("MENU");
+    x = COLS/2 - 10;
+    if(menu){
+		addch(box_char(7));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(9)), move(y+1,x);
+		addch(box_char(11)),printw("     GAME START     "),addch(box_char(11)), move(y+2,x);
+		addch(box_char(1));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(3)), move(y+3,x);
+		addch(box_char(7));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(9)), move(y+4,x);
+		attron(COLOR_PAIR(1)),addch(box_char(11)),printw(" ->     EXIT        "),addch(box_char(11)),attroff(COLOR_PAIR(1)), move(y+5,x);
+		addch(box_char(1));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(3));
+	}
+    else{
+		addch(box_char(7));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(9)), move(y+1,x);
+		attron(COLOR_PAIR(1)),addch(box_char(11)),printw(" ->  GAME START     "),addch(box_char(11)),attroff(COLOR_PAIR(1)), move(y+2,x);
+		addch(box_char(1));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(3)), move(y+3,x);
+		addch(box_char(7));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(9)), move(y+4,x);
+		addch(box_char(11)),printw("        EXIT        "),addch(box_char(11)), move(y+5,x);
+		addch(box_char(1));
+		for(int i=0;i<20;i++)addch(box_char(10));
+		addch(box_char(3));
+	}
+	return;
+}
 void display_gameboard() {
     mvprintw(0,0,"*Monospaced font recommended");
 	for(int i=0;i<4;i++)
@@ -97,9 +156,6 @@ void display_gameboard() {
             int y=LINES/2+(i-2)*3, x=COLS/2+(j-2)*5;
 			move(y,x);
 			if(on[0] == i && on[1] == j){
-			    start_color();
-			    init_color(8, 999, 999, 999);
-			    init_pair(1, COLOR_BLACK, 8);
 			    attron(COLOR_PAIR(1));
 				addch(box_char(7)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(9)), move(y+1,x);
 				addch(box_char(11)),printw(" %c ", cards[i][j]),addch(box_char(11)), move(y+2,x);
