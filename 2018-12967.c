0dbaefc43062d8f60e88ad205cd6cd5924a94b4a
mushypeas <qkrwoans2@gmail.com>
2018-12-04 16:52:06 +0900

    Duh

--- 2018-12967.c
+++ 2018-12967.c
@@ -29,6 +29,7 @@ void display(int s);
 void display_menu();
 void display_gameboard();
 int box_char(int x);
+void card_box(card cards, int y, int x);
 
 int main() {	
 
@@ -37,23 +38,6 @@ int main() {
     init_color(8, 999, 999, 999);
     init_pair(1, COLOR_BLACK, 8);
 	srand(time(NULL));
-	pick list[8];
-	for(int i=0; i<8; i++){
-		list[i].num = 2;
-		list[i].name = 'A' + i;
-	}
-	for(int i=0; i<4; i++)
-		for(int j=0; j<4; j++){
-			cards[i][j].name = '\0';
-			cards[i][j].status = 0;
-			while(cards[i][j].name == '\0'){
-				int mere = rand()%8;
-				if(list[mere].num > 0){
-					cards[i][j].name = list[mere].name;
-					list[mere].num--;
-				}
-			}
-		}
 	while(1) {
 		display(status);
 		key_in = getch();
@@ -82,20 +66,6 @@ int main() {
 				else
 					on[1] += 1;
 			}
-			else if(key_in == ' '){
-				if(on[0] == 3){
-					if(on[1] == 3)
-						on[0] = 0, on[1] = 0;
-					else
-						on[1] += 1;
-				}
-				else{
-					if(on[1] == 3)
-						on[0] += 1, on[1] = 0;
-					else
-						on[1] += 1;
-				}
-			}
 			else if(key_in == 'Q' || key_in == 'q')
 				status = 0;
 		}
@@ -104,9 +74,7 @@ int main() {
 				menu=0;
 			else if(key_in == down)
 				menu=1;
-			else if(key_in == ' ')
-				menu?(menu=0):(menu=1);
-			else if(key_in == '\n'){
+			else if(key_in == ' '){
 				if(menu)
 					break;
 				else
@@ -125,7 +93,23 @@ void initialize() {
 	keypad(stdscr, TRUE);
 	curs_set(0);
 	noecho();
-
+	pick list[8];
+	for(int i=0; i<8; i++){
+		list[i].num = 2;
+		list[i].name = 'A' + i;
+	}
+	for(int i=0; i<4; i++)
+		for(int j=0; j<4; j++){
+			cards[i][j].name = '\0';
+			cards[i][j].status = 0;
+			while(cards[i][j].name == '\0'){
+				int mere = rand()%8;
+				if(list[mere].num > 0){
+					cards[i][j].name = list[mere].name;
+					list[mere].num--;
+				}
+			}
+		}
 	display(status);
 }
 
@@ -164,6 +148,19 @@ void display_menu(){
 	}
 	return;
 }
+void card_box(cards[i][j],y,x){
+	if(cards[i][j].status == 0){
+		addch(box_char(7)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(9)), move(y+1,x);
+		addch(box_char(11)),printw(" %c ", cards[i][j].name),addch(box_char(11)), move(y+2,x);
+		addch(box_char(1)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(3));
+	}
+	else if(cards[i][j].status == 1){
+		addch(box_char(7)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(9)), move(y+1,x);
+		addch(box_char(11)),printw("   "),addch(box_char(11)), move(y+2,x);
+		addch(box_char(1)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(3));
+	}
+	return;
+}
 void display_gameboard() {
     mvprintw(0,0,"*Monospaced font recommended");
 	for(int i=0;i<4;i++)
@@ -172,15 +169,11 @@ void display_gameboard() {
 			move(y,x);
 			if(on[0] == i && on[1] == j){
 			    attron(COLOR_PAIR(1));
-				addch(box_char(7)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(9)), move(y+1,x);
-				addch(box_char(11)),printw(" %c ", cards[i][j].name),addch(box_char(11)), move(y+2,x);
-				addch(box_char(1)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(3));
-				attroff(COLOR_PAIR(1));
+			    card_box(cards[i][j],y,x)
+			    attroff(COLOR_PAIR(1));
 			}
 			else{
-				addch(box_char(7)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(9)), move(y+1,x);
-				addch(box_char(11)),printw(" %c ", cards[i][j].name),addch(box_char(11)), move(y+2,x);
-				addch(box_char(1)),addch(box_char(10)),addch(box_char(10)),addch(box_char(10)),addch(box_char(3));
+				card_box(cards[i][j],y,x)
 			}
 		}
 }
